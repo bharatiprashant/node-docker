e5ea8cac14cbce79e0b5743b63dee614730eeca8
bharatiprashant <prashantbharatirajak@gmail.com>
2023-01-11 23:42:20 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const postController = require("../controllers/postController");
+const postController = require("../controllers/PostController");
 
 const router = express.Router();
 
